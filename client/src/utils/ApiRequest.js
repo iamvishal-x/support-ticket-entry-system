d5b3063c84e8747efe5ede8a8904f76b557bbf02
Vishal <workvishal09@gmail.com>
2024-01-19 09:11:37 +0530

    updated domain

--- client/src/utils/ApiRequest.js
+++ client/src/utils/ApiRequest.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://support-ticket-entry-system-pink.vercel.app";
 
 const ApiRequest = async (method, path, data) => {
   try {
